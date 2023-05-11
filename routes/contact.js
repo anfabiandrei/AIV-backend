73b2ced0da1bd2a73181c5ee627a9c70ff727124
ciumofaiav <ciumofaiav@gmail.com>
2023-05-11 12:34:56 -0700

    WIP

--- routes/contact.js
+++ routes/contact.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const contactController = require("../controllers/contactController");
+const contactController      = require("../controllers/contactController");
+const stripeController       = require("../controllers/stripeController");
+const notificationController = require("../controllers/notificationController");
 
 router.post('/contact', contactController.send);
+router.post('/notification', notificationController.send);
+router.post('/create-payment-intent', stripeController.send);
 
 module.exports = router;
